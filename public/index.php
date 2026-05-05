74f92dd812da859d46583e7ac48863f5df9c2104
Kevin <kevindarmawan022@gmail.com>
2026-05-05 09:01:03 +0700

    update

--- public/index.php
+++ public/index.php
@@ -17,4 +17,10 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+if (app()->isLocal()) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 $app->handleRequest(Request::capture());
